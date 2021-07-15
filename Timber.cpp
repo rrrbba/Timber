bab4469cfa7878523b1d8c685fd08a9f7b7ddb5e
Rayven <rayven.burns@gmail.com>
2021-07-15 10:45:35 -0400

    created classes and objects for textures and sprites

--- Timber.cpp
+++ Timber.cpp
@@ -7,8 +7,23 @@ int main()
 	//Create a video mode object
 	VideoMode vm(1920, 1080);
 
-	//Create and open a window for the game
+	//Create and open a window for the game (Timber is the window's name)
 	RenderWindow window(vm, "Timber!!!", Style::Fullscreen);
+
+	//Create a texture to hold a graphic on the GPU (Class object)
+	Texture textureBackground;
+
+	//Load a graphic into the texture
+	textureBackground.loadFromFile("graphics/background.png");
+
+	//Create a sprite (Class object)
+	Sprite spriteBackground;
+
+	//Atach the texture to the sprite
+	spriteBackground.setTexture(textureBackground);
+
+	//Set the spriteBackground to cover the screen
+	spriteBackground.setPosition(0, 0);
 
 	while (window.isOpen())
 	{
